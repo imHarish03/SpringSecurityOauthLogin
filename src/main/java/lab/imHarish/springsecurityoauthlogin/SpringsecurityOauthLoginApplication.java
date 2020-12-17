3152ad49722d46af1a2007015f7e53a716185c4f
imHarish03 <harishhari302@gmail.com>
2020-12-17 20:56:02 +0530

    Initial push

---
+++ src/main/java/lab/imHarish/springsecurityoauthlogin/SpringsecurityOauthLoginApplication.java
@@ -0,0 +1,15 @@
+package lab.imHarish.springsecurityoauthlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+public class SpringsecurityOauthLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityOauthLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lab/imHarish/springsecurityoauthlogin/SpringsecurityOauthLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.imHarish.springsecurityoauthlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityOauthLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
